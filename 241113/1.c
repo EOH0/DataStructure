21a465f6061c6d183a7fd929e25bfb8ede60d120
Seonho Kim <eoho4996@gmail.com>
2024-11-28 17:26:28 +0900

    docs: 241113, fix: 241113/2.c

--- 241113/1.c
+++ 241113/1.c
@@ -16,8 +16,8 @@ void insert(treePointer* node, int k);
 treePointer findMax(treePointer root);
 
 int main() {
-    // FILE* file = fopen("in1.txt", "r");
-    FILE* file = fopen("in2.txt", "r");
+    FILE* file = fopen("in1.txt", "r");
+    // FILE* file = fopen("in2.txt", "r");
     if (file == NULL) {
         printf("file error");
         return 1;
@@ -40,8 +40,8 @@ int main() {
 }
 
 treePointer findMax(treePointer root) {
-    if (root) {
-        findMax(root->rightChild);
+    while(root != NULL && root->rightChild != NULL) {
+        root = root->rightChild;
     }
 
     return root;
@@ -53,25 +53,30 @@ void inorder(treePointer ptr) {
         inorder(ptr->rightChild);
     }
 }
-treePointer modifiedSearch(treePointer node, int k) {
+treePointer modifiedSearch(treePointer node, int k) { // 중복 검증 함수 / 기본적으로 BinarySearchTree라는 전제로 검증이 수행
     treePointer now = NULL;
     while(node != NULL) {
         now = node;
-        if (k == node->key) {
+        if (k == node->key) { // 중복이 있으면 아무것도 추가되지 않음
             return NULL;
         }
-        else if (k < node->key) {
+        else if (k < node->key) { // 넣고자 하는 값이 현재 노드의 값보다 작으면 현재 노드의 왼쪽 서브트리에서 비교하도록 왼쪽 자식으로 보냄
+            // BST이므로 k가 node->key보다 작다면 모든 서브트리를 탐색할 필요없이 작은 부분만 탐색만 해도됨
             node = node->leftChild;
         }
-        else {
+        else { // k > node->key
             node = node->rightChild;
         }
     }
+    // 탐색이 끝난 후 중복이 없으면 탐색 도착지 리턴 (도착지는 새로운 노드의 부모)
+    // while문의 종료는 NULL때문이 맞지만, 그 조건을 while이 있는 문장 하나에서 판단하고, now에 선언이 되진 않음
+    // 그러므로 now는 새로운 값의 부모가 될 노드를 가리킴
     return now;
 }
 void insert(treePointer* node, int k) {
-    treePointer ptr, temp = modifiedSearch(*node, k);
-    if (temp || !(*node)) {
+    treePointer ptr; // 새로운 값을 저장할 노드 포인터 / BST는 크기에 상관없이 들어온 순서대로 무조건 자식 위치에 감 (부모와 자리 교체되는 일이 없음)
+    treePointer temp = modifiedSearch(*node, k); // 새로 저장할 값의 부모가 될 노드
+    if (temp != NULL || (*node) == NULL) {
         ptr = (treePointer)malloc(sizeof(struct tree));
         ptr->key = k;
         ptr->leftChild = ptr->rightChild = NULL;
@@ -80,7 +85,7 @@ void insert(treePointer* node, int k) {
             if (k < temp->key) temp->leftChild = ptr;
             else temp->rightChild = ptr;
         }
-        else {
+        else { // 가장 초기 (트리가 비었을때 )
             *node = ptr;
         }
     }
--- 241113/2.c
+++ 241113/2.c
@@ -2,20 +2,19 @@
 #include <stdlib.h>
 
 typedef struct tree* treePointer;
-typedef struct tree{
+typedef struct tree {
     int key;
     treePointer leftChild, rightChild;
 };
 
 void inorder(treePointer ptr);
-treePointer modifiedSearch(treePointer node, int k);
+treePointer search(treePointer node, int k);
 void insert(treePointer* node, int k);
-void delete(treePointer node, int k);
+void delete(treePointer* node, int k);
 treePointer findMax(treePointer root);
-treePointer DupSearch(treePointer node, int k);
+treePointer findMin(treePointer node);
 
 int main() {
-    // FILE* file = fopen("in3.txt", "r");
     FILE* file = fopen("in4.txt", "r");
     if (file == NULL) {
         printf("file error");
@@ -31,67 +30,19 @@ int main() {
         fscanf(file, "%d", &num);
         insert(&root, num);
     }
-    
+
     fscanf(file, "%d", &size);
     for (int i = 0; i < size; i++) {
         fscanf(file, "%d", &num);
-        delete(root, num);
+        delete(&root, num);
     }
 
     fclose(file);
 
     inorder(root);
+    printf("\n");
 }
 
-void delete(treePointer node, int k) {
-    treePointer ptr = DupSearch(node, k);
-    if (ptr == NULL) {
-        printf("E ");
-        return;
-    } else {
-        printf("S ");
-        treePointer temp = ptr;
-        treePointer LChild = temp->leftChild;
-        treePointer RChild = temp->rightChild;
-        
-        if (LChild == NULL && RChild == NULL) {
-            free(temp);
-
-        } else if (LChild == NULL) {
-            ptr->key = RChild->key;
-            ptr->leftChild = RChild->leftChild;
-            ptr->rightChild = RChild->rightChild;
-
-            free(RChild);
-
-        } else if (RChild == NULL) {
-            ptr->key = LChild->key;
-            ptr->leftChild = LChild->leftChild;
-            ptr->rightChild = LChild->rightChild;
-
-            free(LChild);
-
-        } else {
-            treePointer maxNode = LChild;
-            treePointer maxParent = ptr;
-            
-            while (maxNode->rightChild != NULL) {
-                maxParent = maxNode;
-                maxNode = maxNode->rightChild;
-            }
-            
-            ptr->key = maxNode->key;
-            
-            if (maxParent->rightChild == maxNode) {
-                maxParent->rightChild = maxNode->leftChild;
-            } else {
-                maxParent->leftChild = maxNode->leftChild;
-            }
-            
-            free(maxNode);
-        }
-    }
-}
 void inorder(treePointer ptr) {
     if (ptr) {
         inorder(ptr->leftChild);
@@ -99,51 +50,81 @@ void inorder(treePointer ptr) {
         inorder(ptr->rightChild);
     }
 }
-treePointer DupSearch(treePointer node, int k) {
-    treePointer now = NULL;
-    while(node != NULL) {
-        now = node;
+
+treePointer search(treePointer node, int k) {
+    while (node != NULL) {
         if (k == node->key) {
-            return now;
-        }
-        else if (k < node->key) {
+            return node;
+        } else if (k < node->key) {
             node = node->leftChild;
-        }
-        else {
+        } else {
             node = node->rightChild;
         }
     }
     return NULL;
 }
-treePointer modifiedSearch(treePointer node, int k) {
-    treePointer now = NULL;
-    while(node != NULL) {
-        now = node;
-        if (k == node->key) {
-            return NULL;
-        }
-        else if (k < node->key) {
-            node = node->leftChild;
-        }
-        else {
-            node = node->rightChild;
-        }
-    }
-    return now;
-}
+
 void insert(treePointer* node, int k) {
-    treePointer ptr, temp = modifiedSearch(*node, k);
-    if (temp || !(*node)) {
-        ptr = (treePointer)malloc(sizeof(struct tree));
-        ptr->key = k;
-        ptr->leftChild = ptr->rightChild = NULL;
-        
-        if (*node) {
-            if (k < temp->key) temp->leftChild = ptr;
-            else temp->rightChild = ptr;
+    if (*node == NULL) {
+        treePointer newNode = (treePointer)malloc(sizeof(struct tree));
+        newNode->key = k;
+        newNode->leftChild = newNode->rightChild = NULL;
+        *node = newNode;
+        return;
+    }
+
+    if (k < (*node)->key) {
+        insert(&(*node)->leftChild, k);
+    } else if (k > (*node)->key) {
+        insert(&(*node)->rightChild, k);
+    }
+}
+
+void delete(treePointer* node, int k) {
+    if (*node == NULL) {
+        printf("E ");
+        return;
+    }
+
+    if (k < (*node)->key) { // 삭제하고자 하는 값이 현재 노드의 key보다 작으면 왼쪽 서브트리 탐색
+        delete(&(*node)->leftChild, k); // 재귀적으로 다음 서브트리 탐색
+    } else if (k > (*node)->key) { // 삭제하고자 하는 값이 현재 노드의 key보다 크면 오른쪽 서브트리 탐색
+        delete(&(*node)->rightChild, k);
+    } else { // 삭제하고자 하는 값을 트리에서 찾았을떄
+        printf("S ");
+        treePointer temp = *node; // 삭제할 노드를 temp에 할당
+
+        // Case 1: No children (leaf node)
+        if (temp->leftChild == NULL && temp->rightChild == NULL) {
+            *node = NULL; // 그냥 노드를 NULL로 바꾸고
+            free(temp); // node = temp free 해줌
         }
+        // Case 2: One child
+        else if (temp->leftChild == NULL || temp->rightChild == NULL) { 
+            treePointer child = (temp->leftChild != NULL) ? temp->leftChild : temp->rightChild; // 둘 중 NULL이 아닌 쪽을 child로 선언
+            *node = child; // 자식을 부모에 할당
+            free(temp); // temp free 해줌
+        }
+        // Case 3: Two children
         else {
-            *node = ptr;
+            treePointer maxNode = findMax(temp->leftChild); // 자식이 두개면 왼쪽 서브트리에서 가장 큰 값을 부모로 교체함
+            // BST의 구조를 따르려면 부모가 오른쪽 서브트리보다 크면 안되므로 부모보다 작은 서브트리 중 가장 큰 값으로 교체
+            temp->key = maxNode->key; // 어차피 교체를 해야하니까 free는 하지말고, 그냥 값만 바꿔줌
+            delete(&temp->leftChild, maxNode->key); // 삭제한 노드의 자식 중 최대인 값을 복사해왔기 때문에 원본을 삭제해줌
         }
     }
 }
+
+treePointer findMax(treePointer root) {
+    while (root != NULL && root->rightChild != NULL) {
+        root = root->rightChild;
+    }
+    return root;
+}
+
+// treePointer findMin(treePointer node) {
+//     while (node != NULL && node->leftChild != NULL) {
+//         node = node->leftChild;
+//     }
+//     return node;
+// }
