2c4687b92f0a8829233f77bdc1c94e0505a91d7f
Seonho Kim <eoho4996@gmail.com>
2024-12-04 23:28:10 +0900

    docs: 241120_TestInfo(bfs, dfs, insertionSort)

--- 241120/1.c
+++ 241120/1.c
@@ -68,6 +68,7 @@ int main() {
     int num1, num2;
 
     // bfs
+    printf("bfs:\n");
     scanf("%d", &num1);
     scanf("%d", &num2);
     
@@ -77,6 +78,7 @@ int main() {
     init(size);
 
     // dfs
+    printf("dfs:\n");
     scanf("%d", &num1);
     scanf("%d", &num2);
 
@@ -91,16 +93,6 @@ void init(int size) {
         visited[i] = FALSE;
     }
     printf("\n");
-}
-void dfs(int v, nodePointer *head) {
-    nodePointer w;
-    visited[v] = TRUE;
-    printf("%d ", v);
-    for (w = head[v]; w; w = w->link) {
-        if (!visited[w->vertex]) {
-            dfs(w->vertex, head);
-        }
-    }
 }
 nodePointer createNode(int vertex) {
     nodePointer newnode = (nodePointer)malloc(sizeof(Node));
@@ -127,17 +119,43 @@ void insertNode(int vertex, nodePointer *head) {
 void bfs(int v, nodePointer *head) {
     nodePointer w;
     front = rear = NULL;
-    printf("%d ", v);
-    visited[v] = TRUE;
-    addq(v);
-    while(front) {
-        v = deleteq();
+    printf("%d ", v); // 시작하는 값 출력
+    visited[v] = TRUE; // 시작하는 값 마킹
+    addq(v); // 큐에 시작 값 추가
+    while(front) { // front가 NULL이 아닐 때까지
+        v = deleteq(); // 현재 queue에 있는 값 
         for (w = head[v]; w; w = w->link) {
             if (!visited[w->vertex]) {
                 printf("%d ", w->vertex);
                 addq(w->vertex);
                 visited[w->vertex] = TRUE;
             }
+        }
+    }
+    /*
+        0 1 2 3 4 5 6 7 이 있다고 가정
+        그래프는 니가 생각하는 그 그래프
+        1. 맨 처음 큐에 0을 넣음 v = NULL / queue: [ 0 ]
+        2. queue에서 0을 delete하고 v에 할당 v = 0 / queue: [ ]
+        3. 0이랑 연결된 노드들을 탐색, 이때 for 반복의 w를 head를 0으로 지정 -> 연결된 리스트 탐색하는데 이점
+            연결된 리스트를 printf하고 queue에 추가 v = 0 / queue: [ 1, 2 ] => front = 1 / rear = 2
+            queue에 추가할때는 연결된 리스트의 값이 존재하더라도 visited가 FALSE인 경우만 추가
+        4. 0 반복 종료하고 queue에서 1을 delete하고 v에 할당 v = 1 / queue: [ 2 ]
+        6. 1에 연결된 노드들을 탐색
+            인접 노드 (1 연결리스트에 연결된 노드 중 방문하지 않은 것) 3, 4 추가 v = 1 / queue: [ 2, 3, 4 ] => front = 2 / rear = 4
+            1에는 0도 연결되어 있으나 visited[0]은 TRUE(방문을 했음)이므로 queue에 추가되지 않음
+        7. 과정을 계속 반복
+        8. for문을 다 돌고 나왔는데 front가 비었다 == for에서 어떤 것도 queue에 추가되지 않았다
+        9. front가 비었으므로 while문의 조건에 의해 종료
+    */
+}
+void dfs(int v, nodePointer *head) {
+    nodePointer w;
+    visited[v] = TRUE;
+    printf("%d ", v);
+    for (w = head[v]; w; w = w->link) {
+        if (!visited[w->vertex]) {
+            dfs(w->vertex, head);
         }
     }
 }
--- 241120/2.c
+++ 241120/2.c
@@ -28,6 +28,10 @@ int main() {
     }
     fclose(file);
 
+    for (int i = 1; i <= size; i++) {
+        printf("%d %s %d\n", User[i].key, User[i].name, User[i].grade);
+    }
+
     insertionSort(User, size);
 
     for (int i = 1; i <= size; i++) {
@@ -36,16 +40,30 @@ int main() {
 }
 
 void insertionSort(element a[], int n) {
-    for (int j = 2; j < n; j++) {
-        element temp = a[j];
-        insert(temp, a, j - 1);
+    for (int i = 2; i < n; i++) {
+        element temp = a[i];
+        insert(temp, a, i - 1);
     }
 }
 void insert(element e, element a[], int i) {
+    // 기본적으로 정렬하려고 가져온 e라는 원소는 배열의 가장 마지막 원소부터 비교하여 들어가기에 적당한 자리를 찾음
+    // 또한, 삽입 정렬은 삽입하고자하는 원소의 앞부분은 이미 정렬이 되어있음을 전제로 동작
+        // 왜 굳이 뒤에서부터 찾지
+        // 삽입하고자 원소는 i번째 원소
+        // i번째 원소를 정렬할떄 1 ~ i - 1 번째 원소는 이미 정렬이 끝난 상태
+        // 그래서 정렬이 된 부분만 비교해서 원소를 넣음
+        // 정렬도 안된 뒷부분을 비교해봤자 하등 쓸모없으니까
+        // 1 ~ i - 1 사이의 원소는 크기 순으로 되어 있으므로
+        // i - 1부터 1까지 탐색하다가 자신보다 작은 원소가 나오면 그때 삽입
     a[0] = e;
-    while(e.key < a[i].key) {
-        a[i + 1] = a[i];
+    // a[0]는 빈자리
+        // 빈자리에 현재 정렬할 원소를 세팅
+    while(e.key < a[i].key) { // 삽입할 원소보다 크면 계속 반복하면서 배열의 앞쪽으로 이동
+        a[i + 1] = a[i]; // 자신보다 값이 큰 원소는 배열의 한칸 뒤로 계속 옮김
+        // 자신이 들어갈 자리를 미리 확보하면서 들어감
+        // 책을 번호 순으로 꽂기 위해서 꽂으려는 책보다 번호가 큰 책을 옆 칸으로 옮기는 작업이랑 같음
         i--;
     }
-    a[i + 1] = e;
+    // 적당한 위치를 찾았다면
+    a[i + 1] = e; // while의 마지막에 실행된 i--를 고려해 i + 1로 해서 삽입
 }
